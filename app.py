4f8aee872c8ba48eda1ec5c6514a0889fd9fc51c
tmittal1 <tmittal1@ucsc.edu>
2022-02-03 19:20:41 -0800

    flask basic working with PISA API

--- app.py
+++ app.py
@@ -8,4 +8,35 @@ app = Flask(__name__)
 def piso():
     # Gonna have to import function from different file 
     # Look into making API 
-    return render_template('index.html', passObjectsLikeThis = False)
+    error = request.args.get('error')
+    message = request.args.get ('message')
+    try:
+        if request.method =="POST":
+            class_number = request.form['class_number']
+            # password = request.form['password']
+            # lang = request.form[' language']
+            # C, conn = connection()
+    except:
+        pass
+    className, meetingPlace, startDate, startTime, endTime, meetingDays, count = pisa(class_number)
+    print(className)
+    if count > 0:
+        gotData = True
+    else:
+        gotData = False
+
+    return render_template('index.html', className = className, meetingPlace = meetingPlace,
+    startDate=startDate, startTime=startTime, meetingDays=meetingDays, gotData=gotData)
+
+@app.route('/login/', methods=[ 'GET', 'POST'])
+def login():
+    error = request.args.get('error')
+    message = request.args.get ('message')
+    try:
+        if request.method =="POST":
+            email = request.form['email']
+            password = request.form['password']
+            lang = request.form[' language']
+            # C, conn = connection()
+    except:
+        pass
