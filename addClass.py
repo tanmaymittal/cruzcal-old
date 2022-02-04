9097412d66a07ad48aaa1d31c8db2ee286398f11
tmittal1 <tmittal1@ucsc.edu>
2022-02-04 10:08:49 -0800

    setting up heroku

--- addClass.py
+++ addClass.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from datetime import * 
 import os.path
-from googleapiclient.discovery import build
+# from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,80 +12,83 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from bs4 import BeautifulSoup as bs, element
 import pandas as pd
 
-def implicit():
-    from google.cloud import storage
+# def implicit():
+#     from google.cloud import storage
 
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
+#     # If you don't specify credentials when constructing the client, the
+#     # client library will look for credentials in the environment.
+#     storage_client = storage.Client()
 
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+#     # Make an authenticated API request
+#     buckets = list(storage_client.list_buckets())
+#     print(buckets)
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+
+# def main():
+#     """Shows basic usage of the Google Calendar API.
+#     Prints the start and name of the next 10 events on the user's calendar.
+#     """
+#     # print(pisa(44365))
+#     creds = None
+#     # The file token.json stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists('token.json'):
+#         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 'credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open('token.json', 'w') as token:
+#             token.write(creds.to_json())
+
+#     service = build('calendar', 'v3', credentials=creds)
+
+#     # Call the Calendar API
+#     # now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+#     more = True
+#     while more:
+#         # print('Do you want to add a class?')
+#         x = input('Do you want to add a class: Yes or No: ')
+#         if x.lower() != 'yes':
+#             more = False
+#             break
+#         class_number = int(input('Enter Class Number: '))
+        
+#         className, meetingPlace, startDate, startTime, endTime, meetingDays, count = pisa(class_number)
+#         className = input('Enter Class Name: ')
+#         # className = 'PHIL 27'
+#         event = {
+#         'summary': f'{className}',
+#         'location' : f'{meetingPlace}',
+#         'start': {
+#             'dateTime': f'{startDate}T{startTime}:00',
+#             'timeZone': 'America/Los_Angeles'
+#         },
+#         'end': {
+#             # 'dateTime': '2021-10-3T16:30:00.000-07:00',
+#             'dateTime': f'{startDate}T{endTime}:00',
+#             'timeZone': 'America/Los_Angeles'
+#         },
+#         'recurrence': [
+#             f"RRULE:FREQ=WEEKLY;BYDAY={meetingDays};COUNT={count}"
+#         #                 
+#         ],
+#         }
+#         # print(event)
+#         event_one = service.events().insert(calendarId='primary', body=event).execute()
+#         print(event_one['summary'].encode('utf-8'), event_one['start']['dateTime'], event_one['end']['dateTime'])
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    # print(pisa(44365))
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    # now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    more = True
-    while more:
-        # print('Do you want to add a class?')
-        x = input('Do you want to add a class: Yes or No: ')
-        if x.lower() != 'yes':
-            more = False
-            break
-        class_number = int(input('Enter Class Number: '))
-        
-        className, meetingPlace, startDate, startTime, endTime, meetingDays, count = pisa(class_number)
-        className = input('Enter Class Name: ')
-        # className = 'PHIL 27'
-        event = {
-        'summary': f'{className}',
-        'location' : f'{meetingPlace}',
-        'start': {
-            'dateTime': f'{startDate}T{startTime}:00',
-            'timeZone': 'America/Los_Angeles'
-        },
-        'end': {
-            # 'dateTime': '2021-10-3T16:30:00.000-07:00',
-            'dateTime': f'{startDate}T{endTime}:00',
-            'timeZone': 'America/Los_Angeles'
-        },
-        'recurrence': [
-            f"RRULE:FREQ=WEEKLY;BYDAY={meetingDays};COUNT={count}"
-        #                 
-        ],
-        }
-        # print(event)
-        event_one = service.events().insert(calendarId='primary', body=event).execute()
-        print(event_one['summary'].encode('utf-8'), event_one['start']['dateTime'], event_one['end']['dateTime'])
+    pass
 
 
 def pisa(class_number): 
--- app.py
+++ app.py
@@ -8,25 +8,31 @@ app = Flask(__name__)
 def piso():
     # Gonna have to import function from different file 
     # Look into making API 
-    error = request.args.get('error')
-    message = request.args.get ('message')
-    try:
-        if request.method =="POST":
-            class_number = request.form['class_number']
-            # password = request.form['password']
-            # lang = request.form[' language']
-            # C, conn = connection()
-    except:
-        pass
-    className, meetingPlace, startDate, startTime, endTime, meetingDays, count = pisa(class_number)
-    print(className)
-    if count > 0:
-        gotData = True
+    # error = request.args.get('error')
+    # message = request.args.get ('message')
+    count = 0
+    # try:
+    if request.method == "POST":
+        class_number = request.form['class_number']
+        # password = request.form['password']
+        # lang = request.form[' language']
+        # C, conn = connection()
+        className, meetingPlace, startDate, startTime, endTime, meetingDays, count = pisa(class_number)
+        # print(className)
+    
+        # className, meetingPlace, startDate, startTime, endTime, meetingDays = 0, 0, 0, 0, 0, 0
+        # pass
+        if count > 0:
+            gotData = True
+        else:
+            gotData = False
+        return render_template('index.html', className = className, meetingPlace = meetingPlace, startDate=startDate, startTime=startTime, meetingDays=meetingDays, gotData=gotData)
     else:
-        gotData = False
+        # GET 
+        return render_template('index.html')
 
-    return render_template('index.html', className = className, meetingPlace = meetingPlace,
-    startDate=startDate, startTime=startTime, meetingDays=meetingDays, gotData=gotData)
+    
+
 
 # @app.route('/login/', methods=[ 'GET', 'POST'])
 # def login():
@@ -40,3 +46,8 @@ def piso():
 #             # C, conn = connection()
 #     except:
 #         pass
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
+
